46d73e1a001458ecd31663ff1a00499ef78552b3
jackliu <jackliulovelt@outlook.com>
2019-01-25 23:29:43 +0800

    change resize method

--- src/common/js/font-size-mixin.js
+++ src/common/js/font-size-mixin.js
@@ -1,14 +1,13 @@
-const  BASE_SCREEN_WIDTH = 375
+const  BASE_SCREEN_WIDTH = 750
 
 export default {
   created() {
     this.windowResizeHandler()
-    // window.addEventListener('resize', this.windowResizeHandler)
   },
   methods: {
     windowResizeHandler() {
       const clientWidth = window.document.body.clientWidth
-      const scale = BASE_SCREEN_WIDTH / clientWidth
+      const scale = clientWidth / BASE_SCREEN_WIDTH * 0.5  
       const viewport = document.querySelector(`meta[name='viewport']`)
       viewport.content = `width=${BASE_SCREEN_WIDTH},initial-scale=${scale},maximum-scale=${scale},minimum-scale=${scale}`
     }
